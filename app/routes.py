09f921cc8eb13e2b948aa306f00d263086f9db02
Emal Khan <ekhan6@horizon.csueastbay.edu>
2023-04-10 21:37:45 -0700

    twelvth commit

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,8 @@ from app.models import User, Post
 from app import db
 from app.forms import RegistrationForm, EditProfileForm, EmptyForm, PostForm
 from datetime import datetime
+from app.forms import ResetPasswordRequestForm
+from app.email import send_password_reset_email
 
 @app.before_request
 def before_request():
@@ -156,3 +158,17 @@ def explore():
         if posts.has_prev else None
     return render_template("index.html", title='Explore', posts=posts.items,
                           next_url=next_url, prev_url=prev_url)
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html',
+                           title='Reset Password', form=form)
