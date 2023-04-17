52d9e86bc2ae8b8c55a31d21e196248ab77a1f72
EKhan11 <emalkhan8@gmail.com>
2023-04-17 12:46:50 -0700

    Email Support (#1)
    * thirteenth commit
    
    * fourteenth commit
    
    ---------
    
    Co-authored-by: Emal Khan <ekhan6@horizon.csueastbay.edu>

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+import os
+from flask_mail import Mail
 
 app = Flask(__name__)
 login = LoginManager(app)
@@ -13,8 +16,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-from logging.handlers import RotatingFileHandler
-import os
+mail = Mail(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
